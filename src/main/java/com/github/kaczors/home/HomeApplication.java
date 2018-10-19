d0ef4986f3c840bb7e6e58dd45e99189edb411f3
Pawel Kaczorowski <pawel.kaczorowski@nzz.ch>
2018-10-19 13:09:44 +0200

    Initial commit

---
+++ src/main/java/com/github/kaczors/home/HomeApplication.java
@@ -0,0 +1,12 @@
+package com.github.kaczors.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeApplication.class, args);
+	}
+}
